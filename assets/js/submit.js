53999fb9f2f57a38563d699ffa6b86ce6bc50d56
sshawarma <ssharma10393@gmail.com>
2020-07-26 13:51:33 -0500

    Added submit.js

--- assets/js/submit.js
+++ assets/js/submit.js
@@ -3,3 +3,13 @@ function submit_form(id) {
     document.getElementById(id).submit();
   }
 }
+
+function process_user(isUser) {
+  if (!isUser) {
+    document.getElementsByName('business')[0].setAttribute('style', 'display: block;')
+    document.getElementsByName('is_business')[0].setAttribute('value', '1');
+  } else {
+    document.getElementsByName('business')[0].setAttribute('style', 'display: hidden;');
+    document.getElementsByName('is_business')[0].setAttribute('value', '0');
+  }
+}
